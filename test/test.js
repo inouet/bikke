774dbbfccb9a0c9aee9a6afed219d297b5913f5d
Taiji Inoue <inudog@gmail.com>
2015-07-14 13:22:33 +0900

    Cleanup code

---
+++ test/test.js
@@ -0,0 +1,36 @@
+var should = require('should');
+
+describe('test', function () {
+
+
+    /*
+    it('add 1+1=2', function () {
+        func.add(1, 1).should.equal(2);
+    });
+    it('add 2+3=5', function () {
+        func.add(2, 3).should.equal(5);
+    });
+    it('add 100+200=300', function () {
+        func.add(100, 200).should.equal(300);
+    });
+    it('minus 2-1=1', function(){
+        func.minus(2,1).should.equal(1);
+    });
+    it('minus 5-2=3', function(){
+        func.minus(5,2).should.equal(3);
+    });
+    it('minus 300-100=200', function(){
+        func.minus(300,100).should.equal(200);
+    });
+    it('object_method_test', function () {
+        var obj = {
+            add: function (a, b) {
+                return a + b;
+            }
+        };
+        obj.add(1, 2).should.equal(3);
+    });
+    */
+
+});
+
